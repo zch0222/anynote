48782ba32ddbaf847bde8b82f59f610abe202e02
zch0222 <yxlmzch@163.com>
2026-05-02 22:24:21 +0800

    fix: restore backend service startup compatibility

--- services/common/anynote-common-security/src/main/java/com/anynote/common/security/config/SecurityConfig.java
+++ services/common/anynote-common-security/src/main/java/com/anynote/common/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.anynote.common.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @author 称霸幼儿园
  */
 @Configuration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
